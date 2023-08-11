1c1b8cf6b64160c09fc4f2ff495fc66b44bceca3
BAHIBrahim <brahim.bahi@ump.ac.ma>
2023-08-11 15:46:52 +0100

    merge corrections

--- MD_365_CRM_FRONT/src/app/components/register/register.component.ts
+++ MD_365_CRM_FRONT/src/app/components/register/register.component.ts
@@ -9,12 +9,13 @@ import { AppStateService } from 'src/app/services/app-state.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+
   /*
-! remove this initialization after debugging is over !
-*/
+  ! remove this initialization after debugging is over !
+  */
   contact: Contact = {
     firstname: "Dan",
     lastname: "Bravo",
@@ -29,10 +30,10 @@ export class RegisterComponent implements OnInit {
 
   loading = false;
 
-
   genderOptions = [
     { value: 'Male', label: 'Male' },
     { value: 'Female', label: 'Female' },
+    // Add more options as needed
   ];
 
   registrationForm!: FormGroup;
@@ -40,7 +41,7 @@ export class RegisterComponent implements OnInit {
   passwordMatch: ValidatorFn = (control: AbstractControl) => {
     const password = control.get('password')?.value;
     const confirmPassword = control.get('confirmPassword')?.value;
-
+    
     if (password !== confirmPassword) {
       return { passwordMismatch: true };
     }
@@ -125,7 +126,7 @@ export class RegisterComponent implements OnInit {
         gendercode: this.registrationForm.get('gender')!.value == 'Male' ? 1 : 0,
         statecode: 1,
         secret: this.contact.secret,
-      }).subscribe(
+      }).subscribe( 
         (response: AuthResponse) => {
 
           if (response.isAuthenticated)
@@ -138,93 +139,8 @@ export class RegisterComponent implements OnInit {
         (error) => {
           console.error('Error occurred while logging:', error);
           this.loading = false;
-    }
-    return null;
-  };
-
-  // Validator 2
-  passwordMatch: ValidatorFn = (control: AbstractControl) => {
-    const password = control.get('password')?.value;
-    const confirmPassword = control.get('confirmPassword')?.value;
-    if (password !== confirmPassword) {
-      return { passwordMismatch: true };
-    }
-    return null;
-  };
-
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private auth: AuthenticationService,
-    private appState: AppStateService
-  ) {
-    this.registrationForm = new FormGroup({
-      firstName: new FormControl('', Validators.required),
-      lastName: new FormControl('', Validators.required),
-      username: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email, this.assertDefault]),
-      password: new FormControl('', [Validators.required,Validators.minLength(8)]),
-      confirmPassword: new FormControl('', [Validators.required,this.passwordMatch]),
-      jobTitle: new FormControl('', Validators.required),
-      gender: new FormControl('', Validators.required),
-    });
-  }
-  ngOnInit(): void {
-    if (
-      this.appState.registrationStep != 3 ||
-      this.appState.contact == null
-    ) {
-      this.appState.registrationStep = -1;
-      this.router.navigate(['']);
-    }
-
-    this.contact = this.appState.contact!;
-    console.log(this.contact);
-    this.registrationForm
-      .get('firstName')
-      ?.setValue(this.contact.firstname);
-    this.registrationForm.get('lastName')?.setValue(this.contact.lastname);
-    this.registrationForm.get('username')?.setValue(this.contact.username);
-    this.registrationForm
-      .get('email')
-      ?.setValue(this.contact.emailaddress1);
-    this.registrationForm.get('jobTitle')?.setValue(this.contact.jobtitle);
-    this.registrationForm
-      .get('gender')
-      ?.setValue(this.contact.gendercode ? this.contact.gendercode : 1);
-  }
-
-  submit() {
-    if (!this.registrationForm.valid) {
-      return;
-    }
-    this.loading = true;
-
-    this.auth
-      .register(
-        {
-          firstname: this.registrationForm.get('firstName')!.value,
-          lastname: this.registrationForm.get('lastName')!.value,
-          username: this.registrationForm.get('username')!.value,
-          email: this.registrationForm.get('email')!.value,
-          password: this.registrationForm.get('password')!.value,
-          jobtitle: this.registrationForm.get('jobTitle')!.value,
-          gendercode: this.registrationForm.get('gender')!.value,
-          statecode: 1,
-        }
-      )
-      .subscribe(
-        // subscribing to the returned observable
-        (response: AuthResponse) => {
-          // console.log(response)
-          if (response.isAuthenticated)
-            this.appState.setAuthStateLocal(response.token);
-
-          this.router.navigate(['']);
-        },
-        (error) => {
-          console.error('Error occurred while logging:', error);
         }
       );
+
   }
 }
